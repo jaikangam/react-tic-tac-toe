e8e24f7d5e74e13175bf88871fcfc19d1f7fea94
jaikangam <jaikangam12@gmail.com>
2022-06-28 03:30:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, } from 'react';
 import './App.css';
+import Icon from "./components/Icons"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Card, CardBody, Container, Button, Col, Row } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+const itemArray = new Array(9).fill("empty")
+
+const App = () => {
+  const [ isCross, setIsCross] = useState(false)
+  const [ winMessage, setWinMessage] = useState("")
+
+  const reloadGame = () => {
+
+  }
+
+  const checkIsWinner = () => {
+
+  }
+
+  const changeItem = itemNumber => {
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Icon />
+        
       </header>
     </div>
   );
